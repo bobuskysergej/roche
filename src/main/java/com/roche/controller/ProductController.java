eeb75e1a4b9024752ffc6607a3efee426abbd945
Sergej Bobusky <sergej.bobusky@ef.com>
2020-10-08 17:35:42 +0200

    init commit

--- src/main/java/com/roche/controller/ProductController.java
+++ src/main/java/com/roche/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.roche.controller.dto.ProductDto;
 import com.roche.controller.mapper.ProductDtoToProductMapper;
 import com.roche.domain.Product;
 import com.roche.service.ProductService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,11 +30,14 @@ public class ProductController {
         this.service = service;
     }
 
+    @ApiOperation(value = "Creates a new product", notes = "This endpoint creates a new product in the database.")
     @PostMapping
     public void create(@RequestBody ProductDto productDto) {
         service.create(productMapper.toProduct(productDto));
     }
 
+    @ApiOperation(value = "Updates an exiting product", notes = "This endpoint updates the properties of a product in the database. Anything can be " +
+            "updated except for the product sku.")
     @PostMapping("{id}")
     public void update(@RequestBody ProductDto productDto, @PathVariable("id") String productId) {
         Product product = productMapper.toProduct(productDto);
@@ -42,16 +45,19 @@ public class ProductController {
         service.update(product);
     }
 
+    @ApiOperation(value = "Retrieves the details of a product", notes = "This endpoint retrieves the details of a product from the database.")
     @GetMapping("{id}")
     public ProductDto get(@PathVariable("id") String productId) {
         return productMapper.toProductDto(service.get(productId));
     }
 
+    @ApiOperation(value = "Retrieves the details of all products", notes = "This endpoint retrieves the details of all products from the database.")
     @GetMapping
     public List<ProductDto> getAll() {
         return service.getAll().stream().map(product -> productMapper.toProductDto(product)).collect(Collectors.toList());
     }
 
+    @ApiOperation(value = "Deletes a product from the database", notes = "The product is actually not deleted but rather a delete flag is set.")
     @DeleteMapping("{id}")
     public void deleteById(@PathVariable("id") String productId) {
         service.deleteById(productId);
